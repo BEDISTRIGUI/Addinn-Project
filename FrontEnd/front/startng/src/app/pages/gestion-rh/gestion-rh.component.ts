085f0df3db2f69eb317b09bf4413047412005276
BEDISTRIGUI <100165263+BEDISTRIGUI@users.noreply.github.com>
2022-07-19 09:59:54 +0100

    template
    menu

---
+++ FrontEnd/front/startng/src/app/pages/gestion-rh/gestion-rh.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-rh',
+  templateUrl: './gestion-rh.component.html',
+  styleUrls: ['./gestion-rh.component.scss']
+})
+export class GestionRHComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ FrontEnd/front/startng/src/app/pages/tools/times-off/times-off.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-times-off',
+  templateUrl: './times-off.component.html',
+  styleUrls: ['./times-off.component.scss']
+})
+export class TimesOffComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
